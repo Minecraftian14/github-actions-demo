449dc3ff7683f550d6f6479c29980d9a36219009
Anirudh Sharma <mcxiv14@gmail.com>
2026-09-20 10:20:51 +0000

    Fixed some config issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -6,8 +5,6 @@ import './App.css'
 import { Counter } from './Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <section id="center">
